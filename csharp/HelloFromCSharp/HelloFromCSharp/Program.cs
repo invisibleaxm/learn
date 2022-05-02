cb15e9bb499fa64457dafc4f8ef3c4ca2c19c819
Alex Campos <alex.campos@gmail.com>
2022-05-02 13:33:59 -0500

    adding learn files

---
+++ csharp/HelloFromCSharp/HelloFromCSharp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello everybody");
+
+Console.WriteLine("This is a mesage from neovim");
+
+Console.WriteLine("Another line");
