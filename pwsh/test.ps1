0596a61ead8ba320664f37d542d724d7149af2f8
Alex Campos <alex.campos@gmail.com>
2022-05-07 09:43:36 -0500

    removed the now ignored directories bin and debug

--- pwsh/test.ps1
+++ pwsh/test.ps1
@@ -1,7 +1,14 @@
 #!/usr/local/bin/pwsh
+function HelloWorld {
+  
+    param(
+    [parameter(Mandatory)]
+    [string]$Greeting
+    )
+    Write-Output "$Greeting World!"
+  }
+  #Calling function HelloWorld
+  HelloWorld -Greeting "Hello"
 
-Write-Output "Hello World"
-$subscriptionId = (New-Guid)
-Write-Output "Your new guid is : $subscriptionId"
 
 #This is a test
