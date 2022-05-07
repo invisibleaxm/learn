9cd6a8e6d637bcf835f949d7d003a3fd6099860c
Alex Campos <alex.campos@gmail.com>
2022-05-07 10:07:18 -0500

    working on git ignores still

--- csharp/HelloFromCSharp/HelloFromCSharp/Program.cs
+++ csharp/HelloFromCSharp/HelloFromCSharp/Program.cs
@@ -2,11 +2,6 @@
 Console.WriteLine("Hello everybody");
 
 Console.WriteLine("Please Enter your Name");
-string? name = Console.ReadLine();
 
-Console.WriteLine("Please enter your last name");
-string? last = Console.ReadLine();
-Console.WriteLine("Hello World from Lsp");
 
-Console.WriteLine("Hello " + name + " " + last);
 
