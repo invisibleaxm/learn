db408c74f8e24f169d9b712e125362e7686aecd7
Alex Campos <alex.campos@ensono.com>
2022-05-12 15:58:26 -0500

    synching from windows

--- csharp/AlexPieShopHRM/AlexPieShopHRM/Program.cs
+++ csharp/AlexPieShopHRM/AlexPieShopHRM/Program.cs
@@ -26,9 +26,70 @@ string name = string.Empty;
 char a = 'a';
 char.IsLower(a);
 
-DateTime employeeStartDate = new DateTime(2025, 03, 28);
+DateTime hireDate = new DateTime(2023, 03, 28);
+Console.WriteLine(hireDate.ToLongDateString());
 
+DateTime exitDate = new DateTime(2025, 12, 11);
+DateTime startDate = hireDate.AddDays(15);
+
+DateTime currentDate = DateTime.Now;
+
+DateTime startHour = DateTime.Now;
+TimeSpan workTime = new TimeSpan(8, 35, 0);
+DateTime endHour = startHour.Add(workTime);
+
+Console.WriteLine(startHour.ToLongDateString());
+Console.WriteLine(endHour.ToShortTimeString());
 
 Console.ReadLine();
 
+var age = 23;
+switch(age)
+{
+    case < 18:
+        Console.WriteLine("Too young to apply");
+        break;
+    case > 65:
+        Console.WriteLine("Too old to apply");
+        break;
+    case 42:
+        Console.WriteLine("Wow, exactly what we are looking for");
+        break;
+    default:
+        Console.WriteLine("Great you can continue");
+        break;
+}
 
+
+Console.WriteLine("Enter a value: ");
+int max = int.Parse(Console.ReadLine());
+int i = 0;
+while (i < max)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+Console.WriteLine("Finished Loop");
+
+Console.WriteLine("Choose the action you want to do: ");
+Console.WriteLine("1. Add employee");
+Console.WriteLine("2. Update employee");
+Console.WriteLine("3. Delete employee");
+string selectedAction = Console.ReadLine();
+
+switch (selectedAction)
+{
+    case "1":
+        Console.WriteLine("Adding new employee...");
+        break;
+    case "2":
+        Console.WriteLine("Updating employee...");
+        break;
+    case "3":
+        Console.WriteLine("Deleting employee...");
+        break;
+    default:
+        Console.WriteLine("InvalidInput");
+        break;
+}
