34e8158afe08d1b997f0018db0167ae823cd3969
Alex Campos <alex.campos@ensono.com>
2022-06-07 08:34:53 -0500

    working on poc

---
+++ csharp/ConsoleApp/ConsoleApp/HR/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.HR
+{
+    internal class Employee
+    {
+        private string _firstName;
+        private double _hourlyRate;
+
+        public Employee(string firstName, double hourlyRate)
+        {
+            HourlyRate = hourlyRate;
+            FirstName = firstName;
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public double HourlyRate
+        {
+            get { return _hourlyRate;  }
+            private set { _hourlyRate = value; }
+        }
+    }
+}
