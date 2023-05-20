73b01b1629ac22dcfeb9b3f78f3fc55ab1a967f4
Alex Campos <alex.campos@gmail.com>
2023-05-20 08:30:45 -0500

    practicing random stuff

--- pwsh/test.ps1
+++ pwsh/test.ps1
@@ -1,17 +1,17 @@
 #!/usr/local/bin/pwsh
 function HelloWorld {
-  
-    param(
+  param(
     [parameter(Mandatory)]
     [string]$Greeting
-    )
-    Write-Output "$Greeting World!"
-  }
-  #Calling function HelloWorld
-  HelloWorld -Greeting "Hello"
+  )
+  Write-Output "$Greeting World!"
+}
+#Calling function HelloWorld
+HelloWorld -Greeting "Hello"
 
 $subscriptionId = (New-Guid)
 Write-Output "Your new guid is : $subscriptionId"
 
 Write-Output "This is a test"
 Write-Output -InputObject "Test"
+
